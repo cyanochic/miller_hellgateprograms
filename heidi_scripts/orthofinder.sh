cd8280c858d4b3999a2bd842a50b12deb5a7f60b
cyanochic <53623936+cyanochic@users.noreply.github.com>
2024-10-04 13:53:31 -0600

    Update orthofinder.sh

--- heidi_scripts/orthofinder.sh
+++ heidi_scripts/orthofinder.sh
@@ -7,11 +7,10 @@
 #SBATCH --cpus-per-task=24
 #SBATCH --mem=96G
 
-netID=ha127954 #your net ID
 orthofinder=~/bin/orthofinder.sif #orthofinder container
 faa=$1 #path to .faa directory
 prefix=$2
-output=$3 #path to desired output directory
+output=$3 #path to desired output directory, WON'T RUN IF DIR ALREADY EXISTS
 
 
-apptainer exec --bind /scratch/${netID}/ --bind /projects/${netID}/  ${orthofinder} orthofinder -f $1 -M msa -oa -n ${prefix} -o ${output}
+apptainer exec --bind /scratch/$USER/ --bind /projects/$USER/  ${orthofinder} orthofinder -f $1 -M msa -oa -n ${prefix} -o ${output}
