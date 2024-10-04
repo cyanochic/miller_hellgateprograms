c6f3ab15e65cb3feb83025c4ac1b5c4ca7cf6266
cyanochic <53623936+cyanochic@users.noreply.github.com>
2024-10-04 13:58:06 -0600

    Update iqtree_genetrees.sh

--- heidi_scripts/iqtree_genetrees.sh
+++ heidi_scripts/iqtree_genetrees.sh
@@ -7,11 +7,10 @@
 #SBATCH --cpus-per-task=24
 #SBATCH --mem=96G
 
-netID=ha127954 #your net ID
 iqtree=~/bin/iqtree2.sif #iqtree2 container
 align=$1
 genes=$2
 prefix=$3
 
 
-apptainer exec --bind /scratch/${netID}/ --bind /projects/${netID}/ ${iqtree} iqtree2 -s ${align} -S ${genes} -B 1000 -alrt 1000 -m TEST -T AUTO --prefix ${prefix}
+apptainer exec --bind /scratch/$USER/ --bind /projects/$USER/ ${iqtree} iqtree2 -s ${align} -S ${genes} -B 1000 -alrt 1000 -m TEST -T AUTO --prefix ${prefix}
