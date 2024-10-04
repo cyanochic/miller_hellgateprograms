4a7e6d4f6ab6f313866654ed1c52b3743372cdae
cyanochic <53623936+cyanochic@users.noreply.github.com>
2024-10-04 14:21:51 -0600

    Update tblastn.sh

--- heidi_scripts/tblastn.sh
+++ heidi_scripts/tblastn.sh
@@ -11,10 +11,9 @@ db=$1
 query=$2
 output=$3
 
-netID=ha127954 #your net ID
 blast=~/bin/blast.sif #blast container
 
 
 ##assumes reference blast genomes already have databases built with makeblastdb
 
-apptainer exec --bind /scratch/${netID}/ --bind /projects/${netID}/ ${blast} tblastn -db ${1} -query ${2} -out ${3} -outfmt 6 -max_hsps 1 -max_target_seqs 10000 -evalue 1 -num_threads 24
+apptainer exec --bind /scratch/$USER/ --bind /projects/$USER/ ${blast} tblastn -db ${1} -query ${2} -out ${3} -outfmt 6 -max_hsps 1 -max_target_seqs 10000 -evalue 1 -num_threads 24
