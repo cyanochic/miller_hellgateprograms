7bc140f515f64f428cb630c564c7dc1dc37514a5
cyanochic <53623936+cyanochic@users.noreply.github.com>
2024-10-04 13:57:39 -0600

    Update iqtree_gcf.sh

--- heidi_scripts/iqtree_gcf.sh
+++ heidi_scripts/iqtree_gcf.sh
@@ -7,11 +7,10 @@
 #SBATCH --cpus-per-task=24
 #SBATCH --mem=96G
 
-netID=ha127954 #your net ID
 iqtree=~/bin/iqtree2.sif #iqtree2 container
 concattree=$1
 locitree=$2
 prefix=$3
 
 
-apptainer exec --bind /scratch/${netID}/ --bind /projects/${netID}/ ${iqtree} iqtree2 -t ${concattree} --gcf ${locitree} --prefix ${prefix}
+apptainer exec --bind /scratch/$USER/ --bind /projects/$USER/ ${iqtree} iqtree2 -t ${concattree} --gcf ${locitree} --prefix ${prefix}
