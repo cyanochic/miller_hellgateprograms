070cacbad26cf17276949dd6274ed94ef6521a64
cyanochic <53623936+cyanochic@users.noreply.github.com>
2024-10-04 13:55:57 -0600

    Update filterblast.sh

--- heidi_scripts/filterblast.sh
+++ heidi_scripts/filterblast.sh
@@ -7,7 +7,6 @@
 
 prefix=$1
 
-netID=ha127954 #your net ID
 blast=~/bin/blast.sif #blast container
 quast=~/bin/quast.sif #quast container
 #fwd=/scratch/ha127954/sc4368/4368_${prefix}_R1.fastq.gz #path to forward raw reads
@@ -19,18 +18,18 @@ mtdb=/projects/ha127954/referenceGenomes/Egib17Bon1_mt.fasta
 
 #egrep -o "NODE\_[[:digit:]]{1,}\_length\_[1-9][[:digit:]]{3,}\_cov\_[1-9][[:digit:]]{0,}\.[[:digit:]]{1,}" scaffolds.fasta > seqIDlist.txt
 
-#apptainer exec --bind /scratch/${netID}/ ${blast} blastdbcmd -entry_batch seqIDlist.txt -db scaffolds.fasta -target_only -out ${prefix}_scaf_filtered.fasta
+#apptainer exec --bind /scratch/$USER/ ${blast} blastdbcmd -entry_batch seqIDlist.txt -db scaffolds.fasta -target_only -out ${prefix}_scaf_filtered.fasta
 
-#apptainer exec --bind /scratch/${netID}/ ${quast} quast.py ${prefix}_scaf_filtered.fasta -o quast/
+#apptainer exec --bind /scratch/$USER/ ${quast} quast.py ${prefix}_scaf_filtered.fasta -o quast/
 
 mkdir localblast/
 #mkdir ${output}selectednodes/
 
 ##assumes reference blast genomes already have databases built with makeblastdb
 
-apptainer exec --bind /scratch/${netID}/ --bind /projects/${netID}/ ${blast} blastn -db ${SBdb} -query ${prefix}_scaf_filtered.fasta -out localblast/${prefix}_blast_SB.txt -outfmt 6 -max_hsps 1
+apptainer exec --bind /scratch/$USER/ --bind /projects/$USER/ ${blast} blastn -db ${SBdb} -query ${prefix}_scaf_filtered.fasta -out localblast/${prefix}_blast_SB.txt -outfmt 6 -max_hsps 1
 
-apptainer exec --bind /scratch/${netID}/ --bind /projects/${netID}/ ${blast} blastn -db ${cpdb} -query ${prefix}_scaf_filtered.fasta -out localblast/${prefix}_blast_cp.txt -outfmt 6 -max_hsps 1
+apptainer exec --bind /scratch/$USER/ --bind /projects/$USER/ ${blast} blastn -db ${cpdb} -query ${prefix}_scaf_filtered.fasta -out localblast/${prefix}_blast_cp.txt -outfmt 6 -max_hsps 1
 
-apptainer exec --bind /scratch/${netID}/ --bind /projects/${netID}/ ${blast} blastn -db ${mtdb} -query ${prefix}_scaf_filtered.fasta -out localblast/${prefix}_blast_mt.txt -outfmt 6 -max_hsps 1
+apptainer exec --bind /scratch/$USER/ --bind /projects/$USER/ ${blast} blastn -db ${mtdb} -query ${prefix}_scaf_filtered.fasta -out localblast/${prefix}_blast_mt.txt -outfmt 6 -max_hsps 1
 
