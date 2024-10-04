0112a31457b7bff4e7e3eb8dbe4ef0c010b0c0b4
cyanochic <53623936+cyanochic@users.noreply.github.com>
2024-10-04 14:03:41 -0600

    Update pgap_sbatch.sh

--- heidi_scripts/pgap_sbatch.sh
+++ heidi_scripts/pgap_sbatch.sh
@@ -6,13 +6,12 @@
 #SBATCH --mem-per-cpu=64G
 
 
-netID=ha127954 #CHANGE THIS TO YOUR NETID
-containername=/scratch/ha127954/pgap_2023-10-03.build7061.sif #put path to your PGAP container
-pgap=/scratch/ha127954/pgap.py #change to where you have pgap.py
+containername=/scratch/$USER/pgap_2023-10-03.build7061.sif #put path to your PGAP container
+pgap=/scratch/$USER/pgap.py #change to where you have pgap.py
 outdir=$1
 genome=$2
 taxid=$3 #taxonomy name from NCBI, must be in single quotes
 
-export PGAP_INPUT_DIR=/scratch/${netID}
+export PGAP_INPUT_DIR=/scratch/$USER
 
-/scratch/${netID}/pgap.py --no-self-update -o ${outdir} -g ${genome} -s ${taxid} -c 2 -r -D singularity --container-name ${containername}
+/scratch/$USER/pgap.py --no-self-update -o ${outdir} -g ${genome} -s ${taxid} -c 2 -r -D singularity --container-name ${containername}
