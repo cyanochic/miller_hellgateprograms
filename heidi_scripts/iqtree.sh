58000ae30952e01a941926676ee3d28542a8ef96
cyanochic <53623936+cyanochic@users.noreply.github.com>
2024-10-04 13:57:14 -0600

    Update iqtree.sh

--- heidi_scripts/iqtree.sh
+++ heidi_scripts/iqtree.sh
@@ -7,10 +7,9 @@
 #SBATCH --cpus-per-task=24
 #SBATCH --mem=96G
 
-netID=ha127954 #your net ID
 iqtree=~/bin/iqtree2.sif #iqtree2 container
 align=$1 #with IQtree2 I think this will take either a file (e.g. SpeciesTreeAlignment.fa) or a directory of fasta files of individual gene alignments? Seems like there's a preference for using a partitioning .nex file to do this
 prefix=$2
 
 
-apptainer exec --bind /scratch/${netID}/ --bind /projects/${netID}/  ${iqtree} iqtree2 -s ${align} -B 1000 -alrt 1000 -m TEST --prefix ${prefix} -T AUTO -nt AUTO
+apptainer exec --bind /scratch/$USER/ --bind /projects/$USER/  ${iqtree} iqtree2 -s ${align} -B 1000 -alrt 1000 -m TEST --prefix ${prefix} -T AUTO -nt AUTO
